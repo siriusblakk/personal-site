0e60ab1afe5edf55c93cac25c6571972594fb9fa
sirusblakk <gunnarmaples@gmail.com>
2024-01-28 10:21:25 -0800

    trim degrees

--- src/data/resume/degrees.js
+++ src/data/resume/degrees.js
@@ -7,12 +7,6 @@ const degrees = [
   },
   {
     school: 'University of Wisconsin-Madison',
-    degree: 'M.S. Mechanical Engineering',
-    link: 'https://engineering.wisc.edu/departments/mechanical-engineering/',
-    year: 2022,
-  },
-  {
-    school: 'University of Wisconsin-Madison',
     degree: 'B.S. Mechanical Engineering',
     link: 'University of Wisconsin-Madison',
     year: 2020,
