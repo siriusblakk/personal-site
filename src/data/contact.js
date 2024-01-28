6f5169b98744667099b8875ade7067079cd1467a
sirusblakk <gunnarmaples@gmail.com>
2024-01-27 22:21:39 -0800

    pass through the pages to get my name right

--- src/data/contact.js
+++ src/data/contact.js
@@ -1,9 +1,5 @@
 import { faGithub } from '@fortawesome/free-brands-svg-icons/faGithub';
-import { faFacebookF } from '@fortawesome/free-brands-svg-icons/faFacebookF';
-import { faInstagram } from '@fortawesome/free-brands-svg-icons/faInstagram';
 import { faLinkedinIn } from '@fortawesome/free-brands-svg-icons/faLinkedinIn';
-import { faAngellist } from '@fortawesome/free-brands-svg-icons/faAngellist';
-import { faTwitter } from '@fortawesome/free-brands-svg-icons/faTwitter';
 import { faEnvelope } from '@fortawesome/free-regular-svg-icons/faEnvelope';
 // See https://fontawesome.com/icons?d=gallery&s=brands,regular&m=free
 // to add other icons.
@@ -15,29 +11,9 @@ const data = [
     icon: faGithub,
   },
   {
-    link: '',
-    label: 'Facebook',
-    icon: faFacebookF,
-  },
-  {
-    link: '',
-    label: 'Instagram',
-    icon: faInstagram,
-  },
-  {
     link: 'https://www.linkedin.com/in/gunnar-m-98055598/',
     label: 'LinkedIn',
     icon: faLinkedinIn,
-  },
-  {
-    link: '',
-    label: 'Angel List',
-    icon: faAngellist,
-  },
-  {
-    link: '',
-    label: 'Twitter',
-    icon: faTwitter,
   },
   {
     link: 'mailto:gunnarmaples@gmail.com',
