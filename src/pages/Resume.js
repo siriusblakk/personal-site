7eb996873f7ae6279f55500e0fb3dbd57ba2de56
sirusblakk <gunnarmaples@gmail.com>
2024-07-31 22:03:06 -0500

    brought resume page back

--- src/pages/Resume.js
+++ src/pages/Resume.js
@@ -24,27 +24,27 @@ const sections = {
 };
 
 const Resume = () => (
-  // <Main
-  //   title="Resume"
-  //   description="Gunnar Maples' Resume."
-  // >
-  //   <article className="post" id="resume">
-  //     <header>
-  //       <div className="title">
-  //         <h2><Link to="resume">Resume</Link></h2>
-  //         <div className="link-container">
-  //           {Object.keys(sections).map((sec) => (
-  //             <h4 key={sec}>
-  //               <a href={`#${sec.toLowerCase()}`}>{sec}</a>
-  //             </h4>))}
-  //         </div>
-  //       </div>
-  //     </header>
-  //     {Object.entries(sections).map(([name, Section]) => (
-  //       <Section key={name} />
-  //     ))}
-  //   </article>
-  // </Main>
+  <Main
+    title="Resume"
+    description="Gunnar Maples' Resume."
+  >
+    <article className="post" id="resume">
+      <header>
+        <div className="title">
+          <h2><Link to="resume">Resume</Link></h2>
+          <div className="link-container">
+            {Object.keys(sections).map((sec) => (
+              <h4 key={sec}>
+                <a href={`#${sec.toLowerCase()}`}>{sec}</a>
+              </h4>))}
+          </div>
+        </div>
+      </header>
+      {Object.entries(sections).map(([name, Section]) => (
+        <Section key={name} />
+      ))}
+    </article>
+  </Main>
 );
 
 export default Resume;
